1066610fad7a8f0a43f2756f9fa67f126c52e454
Michael Stoelting <ms@reflect-media.de>
2022-01-05 16:04:04 +0100

    clear springboot, only the /hello endpoint left to test the application

--- src/main/java/com/clockin/clockin_backend/controller/HomeController.java
+++ src/main/java/com/clockin/clockin_backend/controller/HomeController.java
@@ -1,16 +1,6 @@
 package com.clockin.clockin_backend.controller;
 
-import com.clockin.clockin_backend.authentication.AuthenticationRequest;
-import com.clockin.clockin_backend.authentication.AuthenticationResponse;
-import com.clockin.clockin_backend.authentication.JwtUtil;
-import com.clockin.clockin_backend.authentication.MyUserDetailedService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -20,77 +10,9 @@ import java.util.HashMap;
 @RestController
 public class HomeController {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private MyUserDetailedService userDetailsService;
-
-    @Autowired
-    private JwtUtil jwtTokenUtil;
-
-    @GetMapping(
-            value="/",
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public HashMap<String, String> home() {
-        var list = new HashMap<String, String>();
-        list.put("info", "Demo Info");
-
-        return list;
-    }
-
     @RequestMapping({"/hello"})
     public String hello() {
         return "Hello World";
-    }
-    /*
-    @PostMapping(
-            value = "/authenticate",
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> createAuthenticationToken(@RequestParam(value = "username") String username, @RequestParam(value = "password") String password) throws Exception {
-        var authenticationRequest = new AuthenticationRequest(username, password);
-
-        try {
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
-            );
-        }
-        catch (BadCredentialsException e) {
-            throw new Exception("Incorrect username or password", e);
-        }
-
-
-        final UserDetails userDetails = userDetailsService
-                .loadUserByUsername(authenticationRequest.getUsername());
-
-        final String jwt = jwtTokenUtil.generateToken(userDetails);
-
-        return ResponseEntity.ok(new AuthenticationResponse(jwt));
-    }
-
-     */
-    @PostMapping(
-            value = "/authenticate",
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
-
-        try {
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
-            );
-        }
-        catch (BadCredentialsException e) {
-            throw new Exception("Incorrect username or password", e);
-        }
-
-
-        final UserDetails userDetails = userDetailsService
-                .loadUserByUsername(authenticationRequest.getUsername());
-
-        final String jwt = jwtTokenUtil.generateToken(userDetails);
-
-        return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
 
 }
