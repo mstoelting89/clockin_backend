25e2f35e6cc1aefe17a7b778ca590d00f0e9af2d
Michael Stoelting <ms@reflect-media.de>
2022-01-09 12:33:47 +0100

    clear main branch

--- src/main/java/com/clockin/clockin_backend/model/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.clockin.clockin_backend.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RoleRepository extends CrudRepository<Role, String> {
-
-    Role findByRole(String role);
-
-}
--- src/main/java/com/clockin/clockin_backend/model/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.clockin.clockin_backend.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUsername(String username);
-}
