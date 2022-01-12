ec11595a4609c669f2feb2b475f7d533aa7faeab
Michael Stoelting <ms@reflect-media.de>
2022-01-12 14:45:58 +0100

    - change names from AppUser - add EmailValidator (currently only to return false, later it should validate an email) - add register method which calls a new signUpUser method to add a new user

--- src/main/java/com/clockin/clockin_backend/appuser/AppUserService.java
+++ src/main/java/com/clockin/clockin_backend/appuser/AppUserService.java
@@ -18,4 +18,8 @@ public class AppUserService implements UserDetailsService {
         return appUserRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
     }
+
+    public String signUpUser(AppUser appUser) {
+        return "";
+    }
 }
--- src/main/java/com/clockin/clockin_backend/registration/RegistrationService.java
+++ src/main/java/com/clockin/clockin_backend/registration/RegistrationService.java
@@ -1,10 +1,32 @@
 package com.clockin.clockin_backend.registration;
 
+import com.clockin.clockin_backend.appuser.AppUser;
+import com.clockin.clockin_backend.appuser.AppUserRole;
+import com.clockin.clockin_backend.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "works";
+
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+
+        if (!isValidEmail) {
+            throw new IllegalStateException("email not valid");
+        }
+
+        return appUserService.signUpUser(new AppUser(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.USER
+        ));
     }
 }
