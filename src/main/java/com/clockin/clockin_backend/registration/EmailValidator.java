9e7262d156c90fcb51dd46afab6f33683537eb79
Michael Stoelting <ms@reflect-media.de>
2022-01-12 15:32:16 +0100

    - bugfix in EmailValidator - extend signUpUser method

--- src/main/java/com/clockin/clockin_backend/registration/EmailValidator.java
+++ src/main/java/com/clockin/clockin_backend/registration/EmailValidator.java
@@ -1,10 +1,13 @@
 package com.clockin.clockin_backend.registration;
 
+import org.springframework.stereotype.Service;
+
 import java.util.function.Predicate;
 
+@Service
 public class EmailValidator implements Predicate<String> {
     @Override
     public boolean test(String s) {
-        return false;
+        return true;
     }
 }
